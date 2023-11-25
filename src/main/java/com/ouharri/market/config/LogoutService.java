ec31986bb649f5b0e9c36aab2bae00a2891902e0
ouharri <ouharrioutman@gmail.com>
2023-11-25 20:22:40 +0100

    Service class for handling user logout and token revocation.

--- src/main/java/com/ouharri/market/config/LogoutService.java
+++ src/main/java/com/ouharri/market/config/LogoutService.java
@@ -9,26 +9,39 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for handling user logout and token revocation.
+ */
 @Service
 @RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
 
   private final TokenRepository tokenRepository;
 
+  /**
+   * Handles user logout by revoking the JWT token.
+   *
+   * @param request        HttpServletRequest
+   * @param response       HttpServletResponse
+   * @param authentication Authentication object representing the current user's authentication details
+   */
   @Override
   public void logout(
-      HttpServletRequest request,
-      HttpServletResponse response,
-      Authentication authentication
+          HttpServletRequest request,
+          HttpServletResponse response,
+          Authentication authentication
   ) {
     final String authHeader = request.getHeader("Authorization");
     final String jwt;
-    if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+
+    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
       return;
     }
+
     jwt = authHeader.substring(7);
     var storedToken = tokenRepository.findByToken(jwt)
-        .orElse(null);
+            .orElse(null);
+
     if (storedToken != null) {
       storedToken.setExpired(true);
       storedToken.setRevoked(true);
