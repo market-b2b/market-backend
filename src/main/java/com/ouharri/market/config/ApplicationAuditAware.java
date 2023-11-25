db8572af6d433bb195f073698d5f1d7d297873d4
ouharri <ouharrioutman@gmail.com>
2023-11-25 20:20:02 +0100

    Custom implementation of Spring Data's AuditorAware interface to provide the current user's ID for entity auditing in the application.

--- src/main/java/com/ouharri/market/config/ApplicationAuditAware.java
+++ src/main/java/com/ouharri/market/config/ApplicationAuditAware.java
@@ -1,6 +1,7 @@
 package com.ouharri.market.config;
 
 import com.ouharri.market.entities.User;
+import io.micrometer.common.lang.NonNullApi;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -9,7 +10,18 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * Custom implementation of Spring Data's AuditorAware interface to provide the current user's ID
+ * for entity auditing in the application.
+ */
+@NonNullApi
 public class ApplicationAuditAware implements AuditorAware<UUID> {
+
+    /**
+     * Retrieve the ID of the currently authenticated user as the auditor for entity auditing.
+     *
+     * @return Optional containing the ID of the current user, or empty if no user is authenticated or anonymous.
+     */
     @Override
     public Optional<UUID> getCurrentAuditor() {
         Authentication authentication =
@@ -17,7 +29,7 @@ public class ApplicationAuditAware implements AuditorAware<UUID> {
                         .getContext()
                         .getAuthentication();
         if (authentication == null ||
-            !authentication.isAuthenticated() ||
+                !authentication.isAuthenticated() ||
                 authentication instanceof AnonymousAuthenticationToken
         ) {
             return Optional.empty();
